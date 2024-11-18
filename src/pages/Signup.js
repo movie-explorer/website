17b87edfc3db339c881b228c0ef2c908607aee46
Martti Prittinen <martti.prittinen@gmail.com>
2024-11-18 19:07:48 +0200

    MovieShowtimes first version. Closes #10

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 import '../styles/Login.css';
 import axios from 'axios';
-import jwtDecode from 'jwt-decode';
+
 
 
 export default function Signup() {
---
+++ src/services/api.js
@@ -0,0 +1,39 @@
+import axios from 'axios'
+
+const parseXML = xml => {
+  const parser = new DOMParser()
+  const xmlDoc = parser.parseFromString(xml, 'text/xml')
+  const shows = xmlDoc.getElementsByTagName('Show')
+  const movies = []
+
+  for (let show of shows) {
+    const title = show.getElementsByTagName('Title')[0].textContent
+    const showtime = show.getElementsByTagName('dttmShowStart')[0].textContent
+    const language = show.getElementsByTagName('SpokenLanguage')[0].getElementsByTagName('Name')[0].textContent
+    const subtitles = show.getElementsByTagName('SubtitleLanguage1')[0]?.getElementsByTagName('Name')[0]?.textContent
+    const rating = show.getElementsByTagName('Rating')[0].textContent
+    const genres = show.getElementsByTagName('Genres')[0].textContent.split(', ')
+    const auditorium = show.getElementsByTagName('TheatreAndAuditorium')[0].textContent
+    const link = show.getElementsByTagName('ShowURL')[0].textContent
+    const poster = show.getElementsByTagName('EventLargeImagePortrait')[0].textContent
+
+    movies.push({
+      title,
+      showtime,
+      language,
+      subtitles,
+      rating,
+      genres,
+      auditorium,
+      link,
+      poster
+    })
+  }
+
+  return movies
+}
+
+export const fetchShowtimes = async (date) => {
+  const response = await axios.get(`https://www.finnkino.fi/xml/Schedule/?area=1018&dt=${date}`)
+  return parseXML(response.data)
+}
